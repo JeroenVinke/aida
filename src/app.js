2c7cae94cb9fd027e70ec8d86f314e3f06767332
adriatic <nik@congral.com>
2016-04-25 18:16:11 -0400

    no message

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 export class App {
   configureRouter(config, router) {
     config.title = 'Aida';
+
     config.map([
-      { route: ['edit'],       name: 'edit',        moduleId: 'edit',        nav: true, title: 'Edit' },
-      { route: ['', 'help'],   name: 'help',        moduleId: 'help',        nav: true, title: 'Help' },
+      { route: ['help'],       name: 'help',        moduleId: 'help',        nav: true, title: 'Help' },
       { route: ['file'],       name: 'file',        moduleId: 'file',        nav: true, title: 'File' },
-      { route: ['view'],       name: 'view',        moduleId: 'view',        nav: true, title: 'View' }
-
-
+      { route: ['view'],       name: 'view',        moduleId: 'view',        nav: true, title: 'View' },
+      { route: ['edit'],       name: 'edit',        moduleId: 'edit',        nav: true, title: 'Edit' },
+      { route: ['', 'about'],  name: 'about',       moduleId: 'about',      nav: true, title: 'About' }
     ]);
 
     this.router = router;
