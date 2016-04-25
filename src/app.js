24a26b47571424817813112cab9d2a4823c1e912
adriatic <nik@congral.com>
2016-04-25 18:01:23 -0400

    no message

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ export class App {
     config.title = 'Aida';
     config.map([
       { route: ['edit'],       name: 'edit',        moduleId: 'edit',        nav: true, title: 'Edit' },
-      { route: ['help'],       name: 'help',        moduleId: 'help',        nav: true, title: 'Help' },
+      { route: ['', 'help'],   name: 'help',        moduleId: 'help',        nav: true, title: 'Help' },
       { route: ['file'],       name: 'file',        moduleId: 'file',        nav: true, title: 'File' },
       { route: ['view'],       name: 'view',        moduleId: 'view',        nav: true, title: 'View' }
 
